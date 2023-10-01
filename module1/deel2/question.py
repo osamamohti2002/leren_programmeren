8a7b4699d6529f2091b32718889a49fa8ec07d4a
osamamohti2002 <99078852@mydavinci.nl>
2023-10-02 00:40:41 +0200

    push

---
+++ module1/deel2/question.py
@@ -0,0 +1,6 @@
+naam = input('Hallo, wat is je naam ?')
+leeftijd = input(f'geodendag {naam}, hoe oud ben je?')
+favorite_eten = input(f'En als {leeftijd} jarige, wat eet je het liefst?')
+favorite_drank = input(f'En wat drink je het liefst bij {favorite_eten}?')
+
+print(f'de {leeftijd} jarige {naam} drinkt het liefst {favorite_drank} bij {favorite_eten}')
