40d752801b3cf7bc7dc2013527cc7282daadaac6
osamamohti2002 <99078852@mydavinci.nl>
2023-10-03 21:19:29 +0200

    push

--- module1/deel2/speelhaldag0_2.py
+++ module1/deel2/speelhaldag0_2.py
@@ -1,12 +1,12 @@
-aantal_personen = int(4)
-aantal_minuten = int(45)
-gameseat_per_minuten =int (5)
-
-ticket_kosten = aantal_personen * TOEGANGSTICKET
-gameseat_kosten = (aantal_minuten / gameseat_per_minuten) * GAMESEAT * aantal_personen
-
-total = ticket_kosten + gameseat_kosten
-print(f'een gewldige dagje met {aantal_personen} mensen in de speelhal met {aantal_minuten} minuten VR kost je maar {total.__round__(2)} euro')
+# aantal_personen = int(4)
+# aantal_minuten = int(45)
+# gameseat_per_minuten =int (5)
+#
+# ticket_kosten = aantal_personen * TOEGANGSTICKET
+# gameseat_kosten = (aantal_minuten / gameseat_per_minuten) * GAMESEAT * aantal_personen
+#
+# total = ticket_kosten + gameseat_kosten
+# print(f'een gewldige dagje met {aantal_personen} mensen in de speelhal met {aantal_minuten} minuten VR kost je maar {total.__round__(2)} euro')
 
 
 #prijzen
@@ -17,6 +17,11 @@ gameseat = float(input('hoeveel kost het per 5 minuten?'))
 #aantalen
 aantal_personen = int(input('hoeveel mensen'))
 aantal_minuten = int(input('hoeveel minuten'))
-gameseat_per_minuten =int (5)
+gameseat_per_minuten = 5
 
+#berekening
+ticket_kosten = aantal_personen * toegangsticket
+gameseat_kosten = (aantal_minuten / gameseat_per_minuten) * gameseat * aantal_personen
 
+total = ticket_kosten + gameseat_kosten
+print(f'een gewldige dagje met {aantal_personen} mensen in de speelhal met {aantal_minuten} minuten VR kost je maar {total} euro')
