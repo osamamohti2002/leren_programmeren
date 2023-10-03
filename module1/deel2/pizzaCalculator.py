cf25a985eea76771521384fb32eb4e4fe94fe629
osamamohti2002 <99078852@mydavinci.nl>
2023-10-03 21:37:14 +0200

    push

---
+++ module1/deel2/pizzaCalculator.py
@@ -0,0 +1,13 @@
+# voor naam : Osama
+# acher naam : Mohti
+# opdracht : pizza Calculator
+
+
+#PRIJZEN
+PIZZA_PRIJZEN = {'klein': 7.45 , 'medium': 9.45 , 'groot': 10.99}
+print(f'dat zijn de maten en de prijzen{PIZZA_PRIJZEN} ')
+maat_kiezen = input('welke pizza wilt u hebben ?')
+if maat_kiezen == "klein" or  maat_kiezen == "meduim" or maat_kiezen == "groot":
+    aantal_klein= int(input("Hoeveel keiln pizza'z wilt u betelen?"))
+    totaal = aantal_klein * PIZZA_PRIJZEN['klein']
+
