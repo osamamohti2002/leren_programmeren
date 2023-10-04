b702439debea2c0d43c0478f270d40a387bce1e0
osamamohti2002 <99078852@mydavinci.nl>
2023-10-04 21:34:25 +0200

    push

---
+++ module1/deel3/programma_1.py
@@ -0,0 +1,9 @@
+getal_1 = int(input('voer getal 1 in '))
+getal_2 = int(input('voer getal 2 in'))
+
+if getal_1 > getal_2 :
+    max = getal_1
+    print(f'{getal_1} is het het grootset getal gevolgd door de waarde van max ')
+    print(max)
+else:
+    print('getal b is groter')
