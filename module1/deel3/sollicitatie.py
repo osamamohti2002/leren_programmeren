d4eaafc0e192c2afa78507c6ed79fd42a33d38ab
osamamohti2002 <99078852@mydavinci.nl>
2023-10-09 23:56:39 +0200

    push

--- module1/deel3/sollicitatie.py
+++ module1/deel3/sollicitatie.py
@@ -11,9 +11,29 @@ hoed = input('Ben je in bezit van een hoge hoed?')
 gewicht = float(input('Wat is jou gewicht?'))
 langte = int(input('Hoe lang ben je?'))
 certificaat = input('Heeft Certificaat Overleven met gevaarlijk personeel')
-praktijkervaring_met_dieren_dressuur = int(input('hoe veel jaar/jaren heb je ervaring met praktijkervaring met dieren-dressuur'))
+dieren_dressuur = int(input('hoe veel jaar/jaren heb je ervaring met praktijkervaring met dieren-dressuur'))
 jongleren = int(input('hoe veel jaar ervaring heb je met jongleren'))
 acrobatiek = int(input('hoe veel jaar ervaring heb je met acrobatiek'))
 
 
-vrachtwagen_rijbewijs_ok = vrachtwagen_rijbewijs == 'ja'
+if (vrachtwagen_rijbewijs == 'ja'
+    and hoed == 'ja'
+    and gewicht >= MIN_GEWICHT and gewicht <= MAX_GEWICHT
+    and langte >= MIN_langte and langte <= MAX_LANGTE
+    and certificaat == 'ja'
+    and dieren_dressuur >= 4
+
+):
+    print('gefeliciteerd')
+elif (
+vrachtwagen_rijbewijs == 'ja'
+    and hoed == 'ja'
+    and gewicht >= MIN_GEWICHT and gewicht <= MAX_GEWICHT
+    and langte >= MIN_langte and langte <= MAX_LANGTE
+    and certificaat == 'ja'
+    and jongleren >= 5 or acrobatiek >= 3
+):
+    print('gefeliciteerd 1')
+
+else:
+    print('donderop')
