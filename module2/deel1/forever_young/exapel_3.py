3ffb3e37186114b165c2d47708c470b2fa8fca67
osamamohti2002 <144385904+osamamohti2002@users.noreply.github.com>
2023-11-16 00:18:58 +0100

    push

---
+++ module2/deel1/forever_young/exapel_3.py
@@ -0,0 +1,15 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 3')
+
+# Jouw python instructies zet je vanaf hier:
+for i in range(4):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+robotArm.moveRight()
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
