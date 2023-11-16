3e5cdb1b405cd0c27da252f0f7b8a6adc6a49e0f
osamamohti2002 <99078852@mydavinci.nl>
2023-11-16 09:30:50 +0100

    push

---
+++ module2/deel1/forever_young/exaple_4.py
@@ -0,0 +1,18 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 4')
+
+# Jouw python instructies zet je vanaf hier:
+robotArm.grab()
+def move_right():
+    for i in range(2):
+        robotArm.moveRight()
+    robotArm.drop()
+
+
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
