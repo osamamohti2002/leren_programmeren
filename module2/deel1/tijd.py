52c973a85ea1627a33c9ba54f8ffb6086d2e1e4e
osamamohti2002 <99078852@mydavinci.nl>
2023-11-16 15:23:20 +0100

    push

--- module2/deel1/tijd.py
+++ module2/deel1/tijd.py
@@ -1,19 +1,9 @@
 for uur in range(1, 25):
     # periode = 'AM' if uur < 12 else 'PM'
-    periode = ''
     if uur < 12:
         periode = 'AM'
     else:
         periode = 'PM'
     omgezet_uur = uur if uur == 12 else uur % 12
     print(f"{omgezet_uur:02d}:00 {periode}")
-
-
-#
-# for uur in range(1,25):
-#     if uur <= 12:
-#         print(f'{uur} AM')
-#     else:
-#         uur =
-#         print(f'{uur }PM')
 
