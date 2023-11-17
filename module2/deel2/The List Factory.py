c98e2a2ec39c84d8480e09f3fc622a4aee93f8d8
osamamohti2002 <99078852@mydavinci.nl>
2023-11-17 14:32:55 +0100

    push

--- module2/deel2/The List Factory.py
+++ module2/deel2/The List Factory.py
@@ -2,9 +2,16 @@ aantal_lijstjes = int(input("Voer het aantal lijstjes in: "))
 
 
 lijst = []
-for i in range(aantal_lijstjes * 1):
+for i in range(aantal_lijstjes):
     langte = int(input('wat is de langte van je lijst'))
     lijst.append(langte)
     print(lijst)
+
+for i in lijst:
+    print(i)
+
+
+
 # stap 2 for loop maken en lengtes vragen -> list
 # stap 3 for elke iests in in de list
+#stap 4 de gegenereerde  langte toevoegen aan de gegenereerde  lijsten
