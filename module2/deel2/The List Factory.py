e0f4ab6bb7294108fc626e1b10f1dc6389977455
osamamohti2002 <99078852@mydavinci.nl>
2023-11-20 16:27:37 +0100

    push

--- module2/deel2/The List Factory.py
+++ module2/deel2/The List Factory.py
@@ -6,25 +6,36 @@
 #
 #     print(LIJST)
 #
+# main_lijst = []
+# aantal_lijsten = int(input('hoe veel lijsten'))
+#
+# for lijst in range(aantal_lijsten):
+#     langte = int(input(f'hoe veel elementen moet er in {lijst}'))
+#     main_lijst.append(lijst)
+#     # print(lijst)
+#     # print(main_lijst)
+#     for i in range(langte):
+#         lijst = []
+#         lijst.append(langte)
+#         print(lijst)
+#     main_lijst.append(lijst)
+#
+#
+# print(main_lijst)
 main_lijst = []
-aantal_lijsten = int(input('hoe veel lijsten'))
-
+aantal_lijsten = int(input('hoeveel lijsten'))
 for lijst in range(aantal_lijsten):
-    langte = int(input(f'hoe veel elementen moet er in {lijst}'))
-    main_lijst.append(lijst)
-    # print(lijst)
+    langte = int(input(f'hoe lang is list {lijst + 1} '))
+    genereerd_lijst = []
+    # print(len(genereerd_lijst))
+    # main_lijst.append(genereerd_lijst)
     # print(main_lijst)
-    for i in range(langte):
-        lijst = []
-        lijst.append(langte)
-        print(lijst)
-    main_lijst.append(lijst)
-
-
+    for ele in range( langte):
+        element = lijst * ele
+        genereerd_lijst.append(element)
+    main_lijst.append(genereerd_lijst)
+    print(genereerd_lijst)
 print(main_lijst)
-
-
-
 
 # for lijst in range(0, aantal_lijsten):
 #     print(main_lijst)
@@ -35,7 +46,6 @@ print(main_lijst)
 # print(len(main_lijst))
 #
 #
-
 
 # main_lijst = []
 # aantal_lijsten = int(input('Hoeveel lijsten: '))
