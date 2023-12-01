f12c2d8148e170cc72218a32560a4354d18dbb3e
osamamohti2002 <99078852@mydavinci.nl>
2023-12-01 20:54:26 +0100

    push

--- module2/deel3/forever_young/example11.py
+++ module2/deel3/forever_young/example11.py
@@ -3,11 +3,26 @@ from RobotArm import RobotArm
 robotArm = RobotArm('exercise 11')
 
 # Jouw python instructies zet je vanaf hier:
-robotArm.grab()
+# robotArm.grab()
+# color = robotArm.scan()
+# if color == 'red':
+#     robotArm.moveRight()
+#     robotArm.drop()
 color = robotArm.scan()
-if color == 'red':
-    robotArm.moveRight()
+for i in range(9):
+    robotArm.grab()
+    robotArm.scan()
+    if color == 'red':
+        print(f'{i} {color}')
     robotArm.drop()
+    robotArm.moveRight()
+    print(color)
+    if color == 'red':
+        robotArm.moveRight()
+        robotArm.drop()
+        print(color)
+    else:
+        print(color)
 
 
 # Na jouw code wachten tot het sluiten van de window:
