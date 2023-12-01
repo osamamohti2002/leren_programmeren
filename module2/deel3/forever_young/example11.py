a0d76354d7763f929bbe877a23e10e9a0b79f3ca
osamamohti2002 <99078852@mydavinci.nl>
2023-12-01 22:27:28 +0100

    push

--- module2/deel3/forever_young/example11.py
+++ module2/deel3/forever_young/example11.py
@@ -2,28 +2,23 @@ from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 11')
 
-# Jouw python instructies zet je vanaf hier:
-# robotArm.grab()
-# color = robotArm.scan()
-# if color == 'red':
-#     robotArm.moveRight()
-#     robotArm.drop()
-color = robotArm.scan()
+for a in range(8):
+    robotArm.moveRight()
+
 for i in range(9):
     robotArm.grab()
-    robotArm.scan()
-    if color == 'red':
-        print(f'{i} {color}')
-    robotArm.drop()
-    robotArm.moveRight()
-    print(color)
-    if color == 'red':
+    color = robotArm.scan()
+    if color == 'white':
         robotArm.moveRight()
         robotArm.drop()
-        print(color)
+        if i < 8:
+            robotArm.moveLeft()
+            robotArm.moveLeft()
     else:
-        print(color)
+        robotArm.drop()
+        if i < 8:
+            robotArm.moveLeft()
 
-
-# Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
+
+
