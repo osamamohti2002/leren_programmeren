476951024d8a5e68226aadc825e6308af78e215f
osamamohti2002 <99078852@mydavinci.nl>
2023-12-04 11:49:44 +0100

    push

--- module2/deel3/forever_young/example9.py
+++ module2/deel3/forever_young/example9.py
@@ -1,14 +1,15 @@
 from RobotArm import RobotArm
 
 robotArm = RobotArm('exercise 9')
-
-for aantalblokjes in range(1, 5):  # stapel
-    for _ in range(aantalblokjes):
-        robotArm.grab()
+for aantalblokjes in range(1, 5): #stapel`
+    for x in range(aantalblokjes):
+        for _ in range(aantalblokjes):
+            robotArm.grab()
         for a in range(5): robotArm.moveRight()
         robotArm.drop()
-        for b in range(5): robotArm.moveLeft()
-    robotArm.moveRight()
+        for b in range(4): robotArm.moveLeft()
+        if x < aantalblokjes - 1:
+            robotArm.moveLeft()
 
 robotArm.wait()
 
