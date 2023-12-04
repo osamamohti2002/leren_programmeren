f5c185495b4ec81da930c9c2ec15187d6c5f141c
osamamohti2002 <99078852@mydavinci.nl>
2023-12-04 12:09:36 +0100

    push

---
+++ module2/deel3/forever_young/example13.py
@@ -0,0 +1,23 @@
+from RobotArm import RobotArm
+# Let op: hier start het anders voor een random level:
+robotArm = RobotArm()
+robotArm.randomLevel(1,7)
+
+# Jouw python instructies zet je vanaf hier:
+i = 1
+
+while 1 > 0:
+    robotArm.grab()
+    color = robotArm.scan()
+    if color != '':
+        for x in range(i):
+            robotArm.moveRight()
+        robotArm.drop()
+        for z in range(i):
+            robotArm.moveLeft()
+        i +=1
+    else:
+        break
+# Na jouw code wachten tot het sluiten van de window:
+
+robotArm.wait()
