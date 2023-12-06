b3d8e17fb114ce1a6c551dcb34a8c0a6c5f1f222
osamamohti2002 <99078852@mydavinci.nl>
2023-12-06 14:12:53 +0100

    push

--- module2/deel4/boodschappenlijst.py
+++ module2/deel4/boodschappenlijst.py
@@ -3,7 +3,7 @@ while True:
     item = input("Voer het boodschappenitem in (of typ 'stop' om te eindigen): ").strip().lower()
     if item == 'stop':
         break
-        
+
     hoeveelheid = input("Voer de hoeveelheid in: ").strip()
     if item in boodschappenlijst:
         boodschappenlijst[item] += int(hoeveelheid)
--- module2/deel4/clubbing.py
+++ module2/deel4/clubbing.py
@@ -6,24 +6,36 @@ DRANKJES = ('cola', 'bier', 'champagne')
 VIP_LIST = ('jeroen', 'jouke', 'rudi')
 
 #bouw hieronder de floowchart na
-leeftijd = int(input('Hoe oud ben je?'))
+
+leeftijd = int(input('hoe oud ben je?'))
 if leeftijd > 18:
-    naam = input('Wat is je naam')
+    naam = input('wat is je naam?')
     if naam in VIP_LIST:
-        print('je naam staat in de vip lijst je kan lekker door')
         if leeftijd >= 21:
-            print('je krijgt een blouwe bandje')
-            drank = input('wat wil je drinken')
+            bandje = 'Blauw'
         else:
-            print('je krijgt een rode bandje')
-            drank = input('wat wil je drinken')
+            bandje = 'rood'
+        print(f'je krijgt van mij een {bandje} bandje')
     else:
         if leeftijd >= 21:
-            print('je krijgt van mij een stempeltje')
-            drank = input('wat wil je drinken')
+            print('je krijgt van mij een stempel')
         else:
-            drank = input('wat wil je drinken')
+            print('je krijgt geen stempel')
+    drank = input('wat wil je drinken')
+    if drank in DRANKJES[0] and naam in VIP_LIST:
+        prijs = PRIJS_COLA
+    elif drank in DRANKJES[1]:
+        prijs = PRIJS_BIER
+        if naam in VIP_LIST or leeftijd >= 21:
+            if naam in VIP_LIST:
+        print('cola')
+
+
+
+
 
 else:
-    print('je mag niet naar binnen')
-    print('probeer over aantal jaar nog eens')
+    print('sorry je mag niet naar binnen')
+    restende_jaren = 19 - leeftijd
+    print(f'Probeer het in {restende_jaren} jaar nog eens')
+# bij drank gebleven
