5c3cd8c36f63ab1f22ca7555e60ad273eb6ae74d
osamamohti2002 <99078852@mydavinci.nl>
2023-12-17 23:29:53 +0100

    push

--- module2/deel4/test_raad_speel.py
+++ module2/deel4/test_raad_speel.py
@@ -1,34 +1,43 @@
 import random
 
-rondes = 20
 scoor = 0
+rondes = 20
+poging = 10
 
-
-
-for ronde in range(1, 21):
+while rondes > 0:
     geheime_getal = random.randint(1, 1000)
-    print(geheime_getal)
-    gok = int(input('gok een getal tussen 1 en 1000'))
-    verschil = abs(geheime_getal - gok)
-    if gok == geheime_getal:
-        rondes -= 1
-        scoor += 1
-        print(f'goed gedaan je hebt nog {rondes} rondes over en je scoor is {scoor}')
+    while poging > 0:
+        gok = int(input('gok een getal'))
+        print(geheime_getal)
+        verschil = abs(geheime_getal - gok)
+        if gok == geheime_getal:
+            scoor += 1
+            rondes -= 1
+            poging -= 10
+            print(f'je hebt dit ronde gewonnen en je moet nog {rondes} te gaan. je scoor is {scoor}')
+        elif verschil < 20:
+            poging -= 1
+            print('je bent heel warm')
+        elif verschil < 50:
+            poging -= 1
+            print('je bent warm')
+        elif gok < geheime_getal:
+            poging -= 1
+            print('hoger')
+        elif gok > geheime_getal:
+            poging -= 1
+            print('lager')
+        else:
+            print(f'je hebt dit ronde verloren je hebt nog {rondes} te gaan')
+            rondes -= 1
+            print(f'de geheime getal was {geheime_getal}')
+    else:
         opnieuw = input('wil je stoppen')
+        poging += 10
         if opnieuw == 'ja':
             break
-    elif verschil < 20:
-        print('je bemn heel warm')
-    elif verschil < 50:
-        print('je bent warm')
-    elif gok < geheime_getal:
-        print('hoger')
-    else:
-        print('lager')
-    if ronde == 10 and scoor == scoor:
-        print('je hebt verloren ')
-        break
-    elif rondes == 0:
-        print('je bent legend je hebt alles geraden')
-        print(f'je scoor {scoor}')
-        break
+    if rondes == 0:
+        print(f'goed gespeeld je scoor is {scoor}')
+    elif rondes == 0 and scoor == 20:
+        print('je bent een lagend je hebt goed gewonne')
+
