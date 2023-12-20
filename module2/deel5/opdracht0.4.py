91846ddb95cbb7906f1389325da650edc2606f4c
osamamohti2002 <99078852@mydavinci.nl>
2023-12-20 13:01:02 +0100

    push

--- module2/deel5/opdracht0.4.py
+++ module2/deel5/opdracht0.4.py
@@ -1,7 +1,7 @@
 from fruitmand import fruitmand
 import random
 
-getal = int(input('voer de aantal fruit in'))
+getal = int(input('voer aantal in '))
 
 for _ in range(getal):
     fruit = random.choice(fruitmand)['name']
