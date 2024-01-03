f5b9ab3d19ecccfd9efb6ff0a88ac52952ee40cc
osamamohti2002 <99078852@mydavinci.nl>
2024-01-03 22:01:56 +0100

    opdracht 10 fruitmand

---
+++ module2/deel5/opdracht10.py
@@ -0,0 +1,7 @@
+from fruitmand import fruitmand
+
+gesorteerde_fruit_op_gewicht = sorted(fruitmand, key=lambda fruit: fruit['weight'], reverse=True )
+
+for fruit in gesorteerde_fruit_op_gewicht:
+    gewicht_in_kg = fruit['weight'] / 1000
+    print(f"{fruit['name']} : {gewicht_in_kg} kg")
