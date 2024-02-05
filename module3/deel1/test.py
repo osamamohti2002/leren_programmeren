0c134c36768d6daaf2c251548a1e72de8c4a8be7
osamamohti2002 <99078852@mydavinci.nl>
2024-02-05 14:32:59 +0100

    figbonacci

--- module3/deel1/test.py
+++ module3/deel1/test.py
@@ -1,13 +1,3 @@
-def fibonacci(aantal_getallen):
-    getallen_reeks = [0, 1]
-
-    for _ in range(2, aantal_getallen):
-        volgende_getaal = getallen_reeks[-1] = getallen_reeks[-2]
-        getallen_reeks.append(volgende_getaal)
-
-        return getallen_reeks
-
-n = int(input("Voer het aantal gewenste getallen in de Fibonacci-reeks in: "))
-fib_result = fibonacci(n)
-
-print("De Fibonacci-reeks tot het {}e getal is: {}".format(n, fib_result))
+fibonacci_reeks = [0, 1]
+volgend_getal = fibonacci_reeks[-1] + fibonacci_reeks[-2]
+print(fibonacci_reeks[-2])
