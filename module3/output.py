c2d299f23fd42248efed75daf304ada40ee46477
osamamohti2002 <99078852@mydavinci.nl>
2024-02-07 12:46:39 +0100

    output

---
+++ module3/output.py
@@ -0,0 +1,5 @@
+from uitbreiding_woonplaats import gegevens_verzamelen
+informatie = gegevens_verzamelen()
+
+for persoon in range(len(informatie)):
+    print(f"{informatie[persoon]['naam']}, die in {informatie[persoon]['city']} woont is {informatie[persoon]['leeftijd']} jaar oud")
